70e5afacde688488ee7e4281ed7e9ff6e7810d1f
Spirito-Libero <ritesh.singh.19a@gmail.com>
2024-07-09 12:12:38 +0530

    Update deployment links in Cors and fetch

--- server/backend/Server.js
+++ server/backend/Server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use(cors(
     {
         // origin: 'http://localhost:3000'
-        origin: 'https://oldnotes-root-frontend.onrender.com/',
+        origin: 'https://oldnotes-root-frontend.onrender.com',
         methods: ["GET", "POST"],
     }
 ));
